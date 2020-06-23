9e70065b8fda9bce8cf06228f4563288b929e242
sunnyguan <sunny.peng.guan@gmail.com>
2020-06-23 16:23:31 -0700

    testing if components are working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/utdng'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/utdng/index.html'));});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/utdng/index.html'));
+});
 app.listen(process.env.PORT || 8080);
