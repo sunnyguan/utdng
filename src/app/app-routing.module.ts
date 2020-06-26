c1d0b10a0cfaaa9ddcfd4a1b69d2e6dea33840bc
sunnyguan <sunny.peng.guan@gmail.com>
2020-06-26 09:20:19 -0700

    some more tweaks to animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SectionsComponent } from './sections/sections.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SectionDetailComponent } from './section-detail/section-detail.component';
+import { SectionSearchComponent } from './section-search/section-search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:sid', component: SectionDetailComponent },
   { path: 'sections/:query', component: SectionsComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'smart', component: SectionSearchComponent }
 ];
 
 @NgModule({
