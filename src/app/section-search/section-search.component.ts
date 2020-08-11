236cefcba2fa57128f126e325ac11d5ae469f135
sunnyguan <sunny.peng.guan@gmail.com>
2020-08-11 10:22:32 -0700

    fixed cs search bug

--- src/app/section-search/section-search.component.ts
+++ src/app/section-search/section-search.component.ts
@@ -57,6 +57,8 @@ export class SectionSearchComponent implements OnInit, AfterViewInit {
 
   submitMe(name: string) {
     // (<HTMLInputElement>document.getElementById('search-box')).value = name;
+    if(name.startsWith("CS "))
+      name = "CS" + name.substr(3);
     this.router.navigate([`/sections/${name}`])
   }
 
