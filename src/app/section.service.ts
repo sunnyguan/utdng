f1cb25721afca61a39e06be5ac0d81969d9d8355
Sunny Guan <sunny.peng.guan@gmail.com>
2020-09-04 10:48:19 -0500

    changed ngrok to windows

--- src/app/section.service.ts
+++ src/app/section.service.ts
@@ -10,7 +10,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class SectionService {
   private sectionsUrl = "api/sections";
-  private prefix = "https://6dc75fd0dfc2.ngrok.io/";
+  private prefix = "https://8a75c4b8ae46.ngrok.io/";
   private APIUrl = this.prefix + "api/coursetest?";
   private scheduleURL = this.prefix + "api/schedule?query=";
   private searchURL = this.prefix + "api/smart";
