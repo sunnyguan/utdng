59b67b80473634a92127c0a408970572077412e0
Sunny Guan <sunny.peng.guan@gmail.com>
2020-09-13 00:19:52 -0500

    raspberry pi server

--- src/app/section.service.ts
+++ src/app/section.service.ts
@@ -10,7 +10,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class SectionService {
   private sectionsUrl = "api/sections";
-  private prefix = "https://3ab48e5ace05.ngrok.io/";
+  private prefix = "https://1b81f7906afb.ngrok.io/";
   private APIUrl = this.prefix + "api/coursetest?";
   private scheduleURL = this.prefix + "api/schedule?query=";
   private searchURL = this.prefix + "api/smart";
